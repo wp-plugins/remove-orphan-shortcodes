7a70dcd3ff59ed7bd843d27e4158f2e5d12502cc
mekshq <mekshq@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-07 23:59:05 +0000

    files added
    git-svn-id: https://plugins.svn.wordpress.org/remove-orphan-shortcodes/trunk@709626 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Remove Orphan Shortcodes ===
+Contributors: MeksHQ
+Donate link: http://mekshq.com/
+Tags: shortcodes, shortcode, php, content, tag, tags, automatic, editor, simple, text
+Requires at least: 3.0
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+
+Quickly remove unused (orphan) shortcode tags from your content.
+
+== Description ==
+
+This is a simple plugin which automatically hides unused (orphan) shortcode tags added by previous themes and/or plugins. You can best use it if you want to switch to another WordPress theme, and avoid manual removal of unused shortcode tags inside your content.
+
+== Installation ==
+
+- Upload remove_orphan_shortcodes.zip to plugins, like any other plugin, or upload unzipped folder remove to wp-content/plugins/
+- Activate the plugin through the "Plugins" menu in WordPress
+
+== Frequently Asked Questions ==
+
+- For any questions, error reports and suggestions please email support@mekshq.com or visit http://mekshq.com
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ remove-orphan-shortcodes.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+Plugin Name: Remove Orphan Shortcodes
+Plugin URI: http://mekshq.com
+Description: Quickly remove unused (orphan) shortcode tags from your content.
+Author: MeksHQ
+Version: 1.0
+Author URI: http://mekshq.com
+*/
+
+if(!function_exists('remove_orphan_shortcodes')){
+
+/* Hook shortcodes removal function to the_content filter */
+add_filter('the_content', 'remove_orphan_shortcodes', 0);
+
+/* Main function which finds and hides unused shortcodes */
+function remove_orphan_shortcodes($content) {
+		global $shortcode_tags;
+		
+		//Check for active shortcodes
+		$active_shortcodes = ( is_array($shortcode_tags) && !empty($shortcode_tags) ) ? array_keys($shortcode_tags) : array();
+		
+		$hack = md5(microtime());
+		$content = str_replace("/",$hack, $content); //avoid "/" chars in content breaks preg_replace
+		
+		if(!empty($active_shortcodes)){
+			//Be sure to keep active shortcodes
+			$keep_active = implode("|", $active_shortcodes);
+			$content= preg_replace( "~(?:\[/?)(?!(?:$keep_active))[^/\]]+/?\]~s", '', $content );
+		} else {
+			//Strip all shortcodes
+			$content = preg_replace("~(?:\[/?)[^/\]]+/?\]~s", '', $content);			
+		}
+		
+		$content = str_replace($hack,"/",$content); // set "/" back to its place
+		
+  	return $content;
+}
+
+}
+
+?>
